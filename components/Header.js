5e8560ea6adf34621ac8dcd1100eab97a0bf1e9d
Habib Afolabi <habibwritescode@gmail.com>
2021-05-07 11:00:32 +0100

    Finish header responsiveness

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,4 @@
 import Button from "./Button";
-import Image from 'next/image';
 
 const Header = () => {
   return (
@@ -7,16 +6,19 @@ const Header = () => {
       <div className='header__text-box'>
         <h1 className='heading-primary'>Live beautifully and comfortably</h1>
         <p className='header__paragraph paragraph'>We design beautiful interiors that are very comfortable</p>
-        <Button name={'Our projects'}/>
+        <Button classN='header__button' name={'Our projects'}/>
       </div>
 
       <div className='header__gallery'>
-        {/* <Image className='header__image' src="/images/inside-weather.png" width={249.74} height={663} /> */}
-        {/* <Image className='header__image' src="/images/minh-pham.png" width={250.87} height={584} alt=""/> */}
-        {/* <Image className='header__image' src="/images/minh-pham2.png" width={249.74} height={490} alt=""/> */}
-        <img src="/images/inside-weather.png" className='header__image header__image--1' alt=""/>
-        <img src="/images/minh-pham.png" className='header__image header__image--2' alt=""/>
-        <img src="/images/minh-pham2.png" className='header__image header__image--3' alt=""/>
+        <div className='header__image-box header__image-box--1'>
+          <img src="/images/inside-weather.png" className='header__image header__image--1' alt=""/>
+        </div>
+        <div className='header__image-box header__image-box--2'>
+          <img src="/images/minh-pham.png" className='header__image header__image--2' alt=""/>
+        </div>
+        <div className='header__image-box header__image-box--3'>
+          <img src="/images/minh-pham2.png" className='header__image header__image--3' alt=""/>
+        </div>        
       </div>
     </header>
   )
