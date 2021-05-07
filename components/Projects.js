92e8acfb5a534a984bcdf69e19aea599aa613d76
Habib Afolabi <habibwritescode@gmail.com>
2021-05-07 13:35:37 +0100

    Finish responsiveness for projects section

--- components/Projects.js
+++ components/Projects.js
@@ -1,24 +1,53 @@
 import React from 'react'
+import useScreenSize from '../hooks/useScreenSize'
+
 import Heading from './Heading'
+import ProjectsCarousel from './ProjectCarousel'
+
 
 const Projects = () => {
+  const size = useScreenSize()
+  
   return (
     <section className='section-projects' id='projects'>
       <div className='section-projects__heading-box'>
         <Heading main={'Projects'} sub={'Latest projects'} />
       </div>
 
+      {size.width < 600 ? <ProjectsCarousel />  
+    
+      : 
+
       <div className='section-projects__gallery-container'>
-        <img src="/images/francesca-tosolini.png" alt="" className="section-projects__image"/>
-        <img src="/images/sidekix-media.png" alt="" className="section-projects__image"/>
-        <img src="/images/id-interiors.png" alt="" className="section-projects__image"/>
-        <img src="/images/chastity-cortijo.png" alt="" className="section-projects__image"/>
-        <img src="/images/devon-janse.png" alt="" className="section-projects__image"/>
-        <img src="/images/sonnie-hiles.png" alt="" className="section-projects__image"/>
-        <img src="/images/yann-maignan.png" alt="" className="section-projects__image"/>
-        <img src="/images/james-mcdonald.png" alt="" className="section-projects__image"/>
-        <img src="/images/cowomen.png" alt="" className="section-projects__image"/>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/francesca-tosolini.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/sidekix-media.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/id-interiors.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/chastity-cortijo.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/devon-janse.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/sonnie-hiles.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/yann-maignan.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/james-mcdonald.png" alt="" className="section-projects__image"/>
+        </div>
+        <div className='section-projetcs__image-box'>
+          <img src="/images/cowomen.png" alt="" className="section-projects__image"/>
+        </div>  
       </div>
+    }
     </section>
   )
 }
